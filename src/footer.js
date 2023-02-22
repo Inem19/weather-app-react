859fd64cf45d326af0680a9a82ae1d3a34326521
Inem19 <117159646+Inem19@users.noreply.github.com>
2023-02-22 18:23:26 +0000

    added open-source to the footer

--- src/footer.js
+++ src/footer.js
@@ -2,5 +2,10 @@ import React from "react";
 import "./footer.css";
 
 export default function Footer() {
-  return <footer>This project was coded by Aniebiet-abasi Akpan.</footer>;
+  return (
+    <footer>
+      This project was coded by Aniebiet-abasi Akpan, and is{" "}
+      <a href="https://github.com/Inem19/weather-app-react">open-sourced.</a>
+    </footer>
+  );
 }
