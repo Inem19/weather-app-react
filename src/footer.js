106120b03fc8ef72fb027043addd754c18b0e0b2
Inem19 <117159646+Inem19@users.noreply.github.com>
2023-03-02 16:40:37 +0000

    added and styled the link to netlify

--- src/footer.js
+++ src/footer.js
@@ -5,7 +5,15 @@ export default function Footer() {
   return (
     <footer>
       This project was coded by <span>Aniebiet-abasi Akpan,</span> and is{" "}
-      <a href="https://github.com/Inem19/weather-app-react">open-sourced.</a>
+      <a href="https://github.com/Inem19/weather-app-react">open-sourced.</a>and
+      <a
+        href="https://idyllic-tarsier-f3064b.netlify.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {" "}
+        hosted on Netlify
+      </a>
     </footer>
   );
 }
